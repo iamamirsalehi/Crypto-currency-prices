44bdcfbf687f2d66bb41b61f01ff0549288e2733
Amir Salehi <iamamirsalehi@gmail.com>
2021-09-21 19:33:35 +0430

    phinix and nobitex implementedf

--- tether.go
+++ tether.go
@@ -10,8 +10,16 @@ import (
 )
 
 type prices struct {
-	name  string
-	price int
+	englishName string
+	persianName string
+	price       int
+}
+
+type crawlType struct {
+	englishName    string
+	persianName    string
+	baseWebsiteUrl string
+	isSellPrice    bool
 }
 
 func GetArzexBestBuyPrices() prices {
@@ -29,7 +37,7 @@ func GetArzexBestBuyPrices() prices {
 	doc, err := goquery.NewDocumentFromReader(resp.Body)
 	checkErr(err)
 
-	prices.name = doc.Find("#RWPCS-usdt-table-buyers tr:nth-child(1) > td:nth-child(1)").First().Text()
+	prices.persianName = doc.Find("#RWPCS-usdt-table-buyers tr:nth-child(1) > td:nth-child(1)").First().Text()
 
 	doc.Find("#RWPCS-usdt-table-buyers tr:nth-child(1) > td:nth-child(2)").Each(func(i int, s *goquery.Selection) {
 		BestBuyPriceForClients := removeTomanFromText(s.Text())
@@ -55,7 +63,7 @@ func GetArzexBestSellPrices() prices {
 	doc, err := goquery.NewDocumentFromReader(resp.Body)
 	checkErr(err)
 
-	prices.name = doc.Find("#RWPCS-usdt-table-buyers tr:nth-child(1) > td:nth-child(1)").First().Text()
+	prices.persianName = doc.Find("#RWPCS-usdt-table-buyers tr:nth-child(1) > td:nth-child(1)").First().Text()
 
 	doc.Find("#RWPCS-usdt-table-sellers tr:nth-child(1) > td:nth-child(2)").Each(func(i int, s *goquery.Selection) {
 		BestSellPriceForClients := removeTomanFromText(s.Text())
@@ -67,64 +75,85 @@ func GetArzexBestSellPrices() prices {
 	return prices
 }
 
-func GetNobitexSellPrices() prices{
-	baseUrl := "https://arzex.io/tether"
-
-	netClient := GetClient()
-
-	resp, err := netClient.Get(baseUrl)
-	checkErr(err)
-	defer resp.Body.Close()
-
-	// Load the HTML document
-	doc, err := goquery.NewDocumentFromReader(resp.Body)
-	checkErr(err)
-
-	price, err := strconv.Atoi(
-		removeTomanFromText(
-			doc.Find("#RWPCS-usdt-table-sellers tr a[href^='https://nobitex.ir/']").Parent().Parent().Find("td:nth-child(2)").Text(),
-		),
-	)
-
-	nobitexSellPrice := prices{
-		name:  "Nobitex",
-		price: price,
+func GetNobitexSellPrices() prices {
+	buyPriceCrawler := crawlType{
+		englishName:    "Nobitex",
+		persianName:    "نوبیتکس",
+		baseWebsiteUrl: "https://nobitex.ir/",
+		isSellPrice:    true,
 	}
 
-	return nobitexSellPrice
+	return base(buyPriceCrawler)
 }
 
-func GetNobitexBuyPrices() prices{
-	baseUrl := "https://arzex.io/tether"
-
-	netClient := GetClient()
-
-	resp, err := netClient.Get(baseUrl)
-	checkErr(err)
-	defer resp.Body.Close()
-
-	// Load the HTML document
-	doc, err := goquery.NewDocumentFromReader(resp.Body)
-	checkErr(err)
-
-	price, err := strconv.Atoi(
-		removeTomanFromText(
-			doc.Find("#RWPCS-usdt-table-buyers tr a[href^='https://nobitex.ir/']").Parent().Parent().Find("td:nth-child(2)").Text(),
-		),
-	)
-
-	nobitexBuyPrice := prices{
-		name:  "Nobitex",
-		price: price,
+func GetNobitexBuyPrices() prices {
+	sellPriceCrawler := crawlType{
+		englishName:    "Nobitex",
+		persianName:    "نوبیتکس",
+		baseWebsiteUrl: "https://nobitex.ir/",
+		isSellPrice:    false,
 	}
+	return base(sellPriceCrawler)
+}
 
-	return nobitexBuyPrice
+func GetPhinixSellPrices() prices {
+	sellPriceCrawler := crawlType{
+		englishName:    "Phinix",
+		persianName:    "فینیکس",
+		baseWebsiteUrl: "https://phinix.ir/",
+		isSellPrice:    true,
+	}
+	return base(sellPriceCrawler)
+}
+
+func GetPhinixBuyPrices() prices {
+	sellPriceCrawler := crawlType{
+		englishName:    "Phinix",
+		persianName:    "فینیکس",
+		baseWebsiteUrl: "https://phinix.ir/",
+		isSellPrice:    false,
+	}
+	return base(sellPriceCrawler)
 }
 
 func checkErr(err error) {
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
+	}
+}
+
+func base(crawlerInfo crawlType) prices {
+	var crawlType string
+
+	baseUrl := "https://arzex.io/tether"
+
+	netClient := GetClient()
+
+	resp, err := netClient.Get(baseUrl)
+	checkErr(err)
+	defer resp.Body.Close()
+
+	// Load the HTML document
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	checkErr(err)
+
+	if crawlerInfo.isSellPrice == true {
+		crawlType = "sellers"
+	} else {
+		crawlType = "buyers"
+	}
+
+	price, err := strconv.Atoi(
+		removeTomanFromText(
+			doc.Find("#RWPCS-usdt-table-" + crawlType + " tr a[href^='" + crawlerInfo.baseWebsiteUrl + "']").Parent().Parent().Find("td:nth-child(2)").Text(),
+		),
+	)
+
+	return prices{
+		englishName: crawlerInfo.englishName,
+		persianName: crawlerInfo.persianName,
+		price:       price,
 	}
 }
 
